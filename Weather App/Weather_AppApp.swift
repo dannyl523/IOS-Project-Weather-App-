e9adad13715b965b7357224d1af98cb0637e632c
BT_C09_12 <mobileappdev@bths.edu>
2026-05-01 14:38:18 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Student on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
