fa963ae5762c874e1fa2a30c943d76b0c298456d
BT_C09_05 <mobileappdev@bths.edu>
2026-05-08 14:40:07 -0400

    reverted city back

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 .padding()
         }
         .onAppear {
-            viewModel.fetchWeather(for: "New York City, NY")
+            viewModel.fetchWeather(for: "New York")
         }
     }
 }
