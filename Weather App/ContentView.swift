d0c071a7b2f3233a57195457776ac3cb5bd738df
BT_C09_12 <mobileappdev@bths.edu>
2026-05-17 22:20:12 -0400

    Truncated the numbers. Created a new DetailView, utilizing sheets as an example of navigation to use already pulled data as a "more details". In that "More Details", there are Winde Degrees, Sunrise and Sunset. I created code to format the Unix format to EST.

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var viewModel = WeatherViewModel()
+    @State private var showDetail = false
 
     var body: some View {
         VStack(spacing: 20) {
@@ -18,13 +19,20 @@ struct ContentView: View {
 
             if let weather = viewModel.weather {
                 VStack(spacing: 10) {
-                    Text("Temperature: \(weather.temp)°C")
-                    Text("Feels Like: \(weather.feels_like)°C")
+                    Text("Temperature: \(String(format: "%.2f", weather.temp))°C")
+                    Text("Feels Like: \(String(format: "%.2f", weather.feels_like))°C")
                     Text("Humidity: \(weather.humidity)%")
-                    Text("Wind: \(weather.wind_speed) m/s")
+                    Text("Wind: \(String(format: "%.2f", weather.wind_speed)) m/s")
                     Text("Cloud Cover: \(weather.cloud_pct)%")
                 }
                 .font(.title3)
+                
+                Button("More Details"){
+                    showDetail = true
+                }
+                .sheet(isPresented: $showDetail){
+                    DetailView(weather: weather)
+                }
             } else if let error = viewModel.errorMessage {
                 Text(error)
                     .foregroundColor(.red)
---
+++ Weather App/DetailView.swift
@@ -0,0 +1,38 @@
+//
+//  DetailView.swift
+//  Weather App
+//
+//  Created by Danny Li on 5/17/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let weather: Weather
+    
+    var timeFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.timeZone = TimeZone(identifier: "America/New_York")
+        return formatter
+    }
+    
+    func unixToEST(_ unix: Int) -> String {
+      let date = Date(timeIntervalSince1970: TimeInterval(unix))
+      return timeFormatter.string(from: date)
+    }
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("More Details")
+                .font(.largeTitle)
+                .bold()
+
+            Text("Wind Degrees: \(weather.wind_degrees)°")
+            Text("Sunrise: \(unixToEST(weather.sunrise))")
+            Text("Sunset: \(unixToEST(weather.sunset))")
+        }
+        .font(.title3)
+        .padding()
+    }
+}
